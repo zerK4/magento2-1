0cad8c80d7475329ea85fd8e522a2371c0fd8a49
zerK4 <sebastian.v.pavel@gmail.com>
2023-06-05 09:43:00 +0300

    first commit

---
+++ cma.js
@@ -0,0 +1,14 @@
+/** @type {import('@scandipwa/magento-scripts').CMAConfiguration} */
+module.exports = {
+    magento: {
+        first_name: 'Sebastian',
+        last_name: 'Pavel',
+        email: 'sebastian.v.pavel@gmail.com',
+        user: 'admin',
+        password: 'password123..',
+        adminuri: 'admin',
+        mode: 'developer',
+        edition: 'community'
+    },
+    configuration: {}
+}
